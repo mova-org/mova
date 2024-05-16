75a6ff1fc0afe313cc0c78b51db51fa926898f0e
Grisha101 <gambo76799@gmail.com>
2024-05-16 19:17:04 +0300

    init

--- index.js
+++ index.js
@@ -1,93 +1,93 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const frames = document.querySelectorAll(".hp-2-blck1");
-
-    frames.forEach(frame => {
-        frame.addEventListener("mouseover", () => {
-            console.log("Clicked!");
-
-            // setTimeout(frameAnim(frame), 1000)
-            frameAnim(frame);
-        });
-    });
-
-    // function anim(){
-    //     frame.style.animation = "upscale 0.5s linear";
-    // }
-
-    function frameAnim(frame) {
-
-        function limit() {
-        // frame.style.background = "green";
-        frame.style.transform = "scale(1.03)";
-        frame.style.padding = "15px";
-        }
-
-        setTimeout(limit, 700)
-        frame.style.animation = "upscale 0.7s 1"
-        
-    }
-
-    console.log("Lanuched!");
-
-
-
-
-
-
-
-
-
-
-
-//reverse
-
-    frames.forEach(frame => {
-        frame.addEventListener("mouseout", () => {
-            console.log("Clicked!");
-
-            setTimeout(frameAnimRev(frame), 1000)
-            
-        });
-    });
-
-    
-    function frameAnimRev(frame) {
-        
-        function limitRev(){
-        // frame.style.background = "yellow";
-        frame.style.transform = "scale(1)";
-        frame.style.padding = "30px";
-        }
-        setTimeout(limitRev, 300)
-        frame.style.animation = "scale 0.3s 1"
-
-    }
-
-    
-});
-
-
-//reverse
 // document.addEventListener("DOMContentLoaded", function() {
 //     const frames = document.querySelectorAll(".hp-2-blck1");
 
 //     frames.forEach(frame => {
-//         frame.addEventListener("mouseout", () => {
+//         frame.addEventListener("mouseover", () => {
 //             console.log("Clicked!");
-//             // setTimeout(() => {
-//             //     frameAnim(frame);
-//             // }, 500);
+
+//             // setTimeout(frameAnim(frame), 1000)
 //             frameAnim(frame);
 //         });
 //     });
 
+//     // function anim(){
+//     //     frame.style.animation = "upscale 0.5s linear";
+//     // }
+
 //     function frameAnim(frame) {
-//         // frame.querySelector(".frame").style.transform = "scale(1)";
-//         // frame.querySelector(".frame").style.padding = "5px";
+
+//         function limit() {
+//         // frame.style.background = "green";
+//         frame.style.transform = "scale(1.03)";
+//         frame.style.padding = "15px";
+//         }
+
+//         setTimeout(limit, 700)
+//         frame.style.animation = "upscale 0.7s 1"
+        
 //     }
 
-//     console.log("Hey");
+//     console.log("Lanuched!");
+
+
+
+
+
+
+
+
+
+
+
+// //reverse
+
+//     frames.forEach(frame => {
+//         frame.addEventListener("mouseout", () => {
+//             console.log("Clicked!");
+
+//             setTimeout(frameAnimRev(frame), 1000)
+            
+//         });
+//     });
+
+    
+//     function frameAnimRev(frame) {
+        
+//         function limitRev(){
+//         // frame.style.background = "yellow";
+//         frame.style.transform = "scale(1)";
+//         frame.style.padding = "30px";
+//         }
+//         setTimeout(limitRev, 300)
+//         frame.style.animation = "scale 0.3s 1"
+
+//     }
+
+    
 // });
+
+
+// //reverse
+// // document.addEventListener("DOMContentLoaded", function() {
+// //     const frames = document.querySelectorAll(".hp-2-blck1");
+
+// //     frames.forEach(frame => {
+// //         frame.addEventListener("mouseout", () => {
+// //             console.log("Clicked!");
+// //             // setTimeout(() => {
+// //             //     frameAnim(frame);
+// //             // }, 500);
+// //             frameAnim(frame);
+// //         });
+// //     });
+
+// //     function frameAnim(frame) {
+// //         // frame.querySelector(".frame").style.transform = "scale(1)";
+// //         // frame.querySelector(".frame").style.padding = "5px";
+// //     }
+
+// //     console.log("Hey");
+// // });
 
 
 
